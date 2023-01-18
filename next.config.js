37b24f9e8f089fbbcbc8cf7b9e409261b1fa62e5
Andrew Wiggin <andrew@wiggin.dev>
2023-01-18 00:55:34 -0500

    Remove file-loader webpack rule

--- next.config.js
+++ next.config.js
@@ -39,14 +39,6 @@ const nextConfig = {
     includePaths: [path.join(__dirname, "src")],
   },
   webpack: (config, { dev }) => {
-    config.module.rules.push({
-      test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|txt)$/,
-      type: "asset/resource",
-      generator: {
-        filename: "static/chunks/[path][name].[hash][ext]",
-      },
-    });
-
     const oneOfRule = config.module.rules.find(
       (rule) => typeof rule.oneOf === "object"
     );
