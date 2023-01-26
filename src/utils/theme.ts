02d968ced7a323d859a22c90c899f0b6e55c0244
Andrew Wiggin <andrew@wiggin.dev>
2023-01-26 16:23:32 -0500

    Revert "Switch order to red -> green -> blue"
    This reverts commit 7f36afa737331498fd030e34bf382df262644d80.

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const DEFAULT_COLOR = "blue";
-export const COLORS = ["red", "green", "blue"] as const;
+export const COLORS = ["blue", "red", "green"] as const;
 
 export const Color = z.enum(COLORS);
 export type Color = z.infer<typeof Color>;
