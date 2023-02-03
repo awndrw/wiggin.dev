f71b1ceead6a679bea6ed6e616d294c083dac312
Andrew Wiggin <andrew@wiggin.dev>
2023-02-03 14:41:08 -0500

    Clean up Palettes classes

--- app/colors/Palettes.tsx
+++ app/colors/Palettes.tsx
@@ -4,22 +4,28 @@ import { COLORS } from "utils/theme";
 import React from "react";
 import styles from "./Palettes.module.scss";
 
-const palettes = {
-  primary: [styles.primary, styles.primaryContrast],
-  container: [styles.secondary, styles.secondaryContrast],
-  tint: [styles.tertiary, styles.tertiaryContrast],
-} as const;
+const palettes = ["primary", "container", "tint"] as const;
 
 export const Palettes = () => {
   return (
     <div className={styles.palettes}>
-      {Object.entries(palettes).map(([name, colorClasses]) => (
+      {palettes.map((name) => (
         <div className={styles.palette} key={name}>
           <div>
             {COLORS.map((color) => (
               <React.Fragment key={color}>
-                <span data-color={color} className={colorClasses[0]} />
-                <span data-color={color} className={colorClasses[1]} />
+                <span
+                  data-color={color}
+                  className={styles.tone}
+                  style={{ backgroundColor: `var(--color-accent-${name})` }}
+                />
+                <span
+                  data-color={color}
+                  className={styles.tone}
+                  style={{
+                    backgroundColor: `var(--color-accent-${name}-contrast)`,
+                  }}
+                />
               </React.Fragment>
             ))}
           </div>
