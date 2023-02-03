c47fc25abfe4990ae47fd1a471484d34c73b6d1e
Andrew Wiggin <andrew@wiggin.dev>
2023-02-03 14:17:46 -0500

    Fix ColorLink rendering error

--- app/ColorLink.tsx
+++ app/ColorLink.tsx
@@ -3,19 +3,21 @@ import styles from "./ColorLink.module.scss";
 
 export const ColorLink = () => (
   <>
-    <Link
-      href="https://webkit.org/blog/10042/wide-gamut-color-in-css-with-display-p3/"
-      type="external"
-      className={styles.displayP3}
-    >
-      Display-P3
-    </Link>
-    <Link
-      href="https://evilmartians.com/chronicles/oklch-in-css-why-quit-rgb-hsl"
-      type="external"
-      className={styles.oklch}
-    >
-      OKLCH
-    </Link>
+    <span className={styles.displayP3}>
+      <Link
+        href="https://webkit.org/blog/10042/wide-gamut-color-in-css-with-display-p3/"
+        type="external"
+      >
+        Display-P3
+      </Link>
+    </span>
+    <span className={styles.oklch}>
+      <Link
+        href="https://evilmartians.com/chronicles/oklch-in-css-why-quit-rgb-hsl"
+        type="external"
+      >
+        OKLCH
+      </Link>
+    </span>
   </>
 );
