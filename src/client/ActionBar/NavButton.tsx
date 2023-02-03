e1c630333b0cebdda0b02cebc6138f0b2d3738b8
Andrew Wiggin <andrew@wiggin.dev>
2023-02-03 14:49:06 -0500

    Add separator to actionbar

--- src/client/ActionBar/NavButton.tsx
+++ src/client/ActionBar/NavButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { AccessibleIcon } from "@radix-ui/react-accessible-icon";
+import { Separator } from "client/Separator";
 import { ActionBarButton } from "client/ActionBar/ActionBarButton";
 import { Icon } from "components/Icon";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
@@ -17,23 +18,31 @@ export default function NavButton() {
   const isBackButton = searchParams.get("ref") === "internal";
 
   return (
-    <ActionBarButton
-      focusArrowColor="initial"
-      focusArrowClassName={styles.focusArrow}
-    >
-      <button
-        className={styles.navButton}
-        style={{
-          borderRadius: isBackButton ? undefined : 0,
-        }}
-        onClick={() => router.push("/")}
-        onMouseOver={() => router.prefetch("/")}
-        role="link"
+    <>
+      <ActionBarButton
+        focusArrowColor="initial"
+        focusArrowClassName={styles.focusArrow}
       >
-        <AccessibleIcon label={isBackButton ? "Back" : "Home"}>
-          <Icon iconName={isBackButton ? "arrow-left" : "home"} />
-        </AccessibleIcon>
-      </button>
-    </ActionBarButton>
+        <button
+          className={styles.navButton}
+          style={{
+            borderRadius: isBackButton ? undefined : 0,
+          }}
+          onClick={() => router.push("/")}
+          onMouseOver={() => router.prefetch("/")}
+          role="link"
+        >
+          <AccessibleIcon label={isBackButton ? "Back" : "Home"}>
+            <Icon iconName={isBackButton ? "arrow-left" : "home"} />
+          </AccessibleIcon>
+        </button>
+      </ActionBarButton>
+      <Separator
+        orientation="vertical"
+        decorative
+        className={styles.separator}
+        style={{ marginRight: isBackButton ? 4 : 2 }}
+      />
+    </>
   );
 }
