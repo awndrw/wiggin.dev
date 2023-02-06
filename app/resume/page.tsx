5a5ba9d78321ae92160b9c4811dd68df929cbd1b
Andrew Wiggin <andrew@wiggin.dev>
2023-02-06 15:30:20 -0500

    Add dark mode resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,10 @@
+import c from "classnames";
 import Image from "next/image";
 import Link from "next/link";
-import resumePage1 from "../../public/resume/page1.jpg";
-import resumePage2 from "../../public/resume/page2.jpg";
+import resumePage1Light from "../../public/resume/page1_light.png";
+import resumePage2Light from "../../public/resume/page2_light.png";
+import resumePage1Dark from "../../public/resume/page1_dark.png";
+import resumePage2Dark from "../../public/resume/page2_dark.png";
 import styles from "./page.module.scss";
 
 export default function Page() {
@@ -14,23 +17,46 @@ export default function Page() {
           target="_blank"
           className={styles.link}
         >
-          <Image
-            src={resumePage1}
-            alt="Resume page 1"
-            priority
-            width={768}
-            className={styles.image}
-            placeholder="blur"
-            aria-hidden
-          />
-          <Image
-            src={resumePage2}
-            alt="Resume page 2"
-            width={768}
-            className={styles.image}
-            placeholder="blur"
-            aria-hidden
-          />
+          <div className={styles.light}>
+            <Image
+              src={resumePage1Light}
+              className={styles.image}
+              priority
+              width={768}
+              placeholder="blur"
+              aria-hidden
+              alt=""
+            />
+            <Image
+              src={resumePage2Light}
+              className={styles.image}
+              priority
+              width={768}
+              placeholder="blur"
+              aria-hidden
+              alt=""
+            />
+          </div>
+          <div className={styles.dark}>
+            <Image
+              src={resumePage1Dark}
+              className={styles.image}
+              priority
+              width={768}
+              placeholder="blur"
+              aria-hidden
+              alt=""
+            />
+            <Image
+              src={resumePage2Dark}
+              className={styles.image}
+              priority
+              width={768}
+              placeholder="blur"
+              aria-hidden
+              alt=""
+            />
+          </div>
         </Link>
       </section>
     </div>
