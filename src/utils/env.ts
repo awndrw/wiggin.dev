ff951debc05c23cd3f8f70683b693624178094de
Andrew Wiggin <andrew@wiggin.dev>
2023-02-13 18:20:08 -0500

    Update env.ts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 export const envSchema = z.object({
   VERCEL_ENV: z.enum(["development", "preview", "production"]),
-  DATOCMS_READONLY_TOKEN: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
