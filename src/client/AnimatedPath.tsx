77fc870c83b70b53eda952617995b9ef55dda619
Andrew Wiggin <andrew@wiggin.dev>
2023-02-17 23:17:01 -0500

    Allow Scribble segment before path

--- src/client/AnimatedPath.tsx
+++ src/client/AnimatedPath.tsx
@@ -11,15 +11,19 @@ export interface AnimatedPathProps {
   d: string;
   /* The ratio of the animated path length to the full path */
   animatedPathRatio: number;
+  startVisible?: boolean;
 }
 
 export const AnimatedPath: React.FC<AnimatedPathProps> = ({
   d,
   animatedPathRatio,
+  startVisible = true,
 }) => {
   const pathRef = React.useRef<SVGPathElement>();
   const [pathLength, setPathLength] = React.useState(0);
   const strokeDashoffset = useSpringValue<number>(0);
+
+  const segmentLength = pathLength / animatedPathRatio;
 
   const recalculatePathLength = () => {
     const path = pathRef.current;
@@ -34,8 +38,11 @@ export const AnimatedPath: React.FC<AnimatedPathProps> = ({
       if (!about) return;
       const scrollProgress =
         1 - about.getBoundingClientRect().top / window.innerHeight;
-      const pathProgress = pathLength * scrollProgress;
-      const startingPosition = pathLength + pathLength / animatedPathRatio;
+      const startingPosition =
+        pathLength + (startVisible ? segmentLength : 2 * segmentLength);
+      const pathProgress =
+        scrollProgress *
+        (startVisible ? pathLength : pathLength + segmentLength);
       strokeDashoffset.start(startingPosition - pathProgress, { immediate });
     };
     recalculateStrokeDashoffset(true);
@@ -59,7 +66,7 @@ export const AnimatedPath: React.FC<AnimatedPathProps> = ({
       style={{
         strokeDashoffset,
         opacity: !pathLength ? 0 : 1,
-        strokeDasharray: `${pathLength / animatedPathRatio} ${pathLength}`,
+        strokeDasharray: `${segmentLength} ${pathLength}`,
       }}
       d={d}
     />
