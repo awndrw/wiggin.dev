c5b90a17d9d54bc40d7f895d21d14c89c5f10012
Andrew Wiggin <andrew@wiggin.dev>
2023-02-21 13:04:06 -0500

    Create /color page (again)

---
+++ app/color/page.tsx
@@ -0,0 +1,83 @@
+import c from "classnames";
+import { Balancer } from "client/Balancer";
+import { Separator } from "client/radix/Separator";
+import { ExternalLink } from "components/Link";
+import { tragedyDisplay } from "fonts/tragedy";
+import styles from "./page.module.scss";
+import { LightnessScale, ChromaScale, HueScale } from "./Scale";
+
+export default function Page() {
+  return (
+    <main className={c(styles.page, tragedyDisplay.className)}>
+      <section className={styles.section}>
+        <div className={styles.hero}>
+          <Balancer>
+            The OKLCH color space, part of{" "}
+            <ExternalLink href="https://www.w3.org/TR/css-color-4">
+              CSS Color Module 4
+            </ExternalLink>
+            , uses Lightness, Chroma, and Hue to describe color.
+          </Balancer>
+        </div>
+        <Separator
+          orientation="horizontal"
+          decorative
+          className={styles.separatorPrimary}
+        />
+        <div className={styles.lchDescription}>
+          <Balancer>
+            Lightness is the <em>perceived</em> brightness of a color (which is
+            different from HSL&apos;s lightness value)
+          </Balancer>
+          <div className={styles.scale}>
+            <LightnessScale />
+          </div>
+        </div>
+        <Separator
+          orientation="horizontal"
+          decorative
+          className={styles.separatorSecondary}
+        />
+        <div className={styles.lchDescription}>
+          <Balancer>
+            Chroma represents the saturation of a color, from grey to fully
+            saturated.
+          </Balancer>
+          <div className={styles.scale}>
+            <ChromaScale />
+          </div>
+        </div>
+        <Separator
+          orientation="horizontal"
+          decorative
+          className={styles.separatorSecondary}
+        />
+        <div className={styles.lchDescription}>
+          <Balancer>
+            Hue is the angle around the color wheel, from 0 to 360 degrees.
+          </Balancer>
+          <div className={styles.scale}>
+            <HueScale />
+          </div>
+        </div>
+      </section>
+      <Separator
+        orientation="horizontal"
+        decorative
+        className={styles.separatorPrimary}
+      />
+      <section className={styles.section}>
+        <Balancer>Bla bla bla</Balancer>
+      </section>
+    </main>
+  );
+}
+
+// It is a perceptually uniform color space, meaning that the distance between
+// two colors is the same as the distance between any two other colors. This is
+// in contrast to the RGB color space, which is not perceptually uniform. The
+// OKLCH color space is also a cylindrical color space, meaning that the color
+// is described by a single angle and two distances from the center of the color
+// space. This is in contrast to the HSL color space, which is a spherical color
+// space, meaning that the color is described by two angles and a distance from
+// the center of the color space.
