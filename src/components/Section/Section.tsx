ecaded942ad7d8bd9d73a52581a6fae92f5ae889
Andrew Wiggin <andrew@wiggin.dev>
2023-03-22 22:35:47 -0400

    Create Section component

---
+++ src/components/Section/Section.tsx
@@ -0,0 +1,38 @@
+import cx from 'classnames';
+import {Separator} from "client/radix/Separator";
+import React from 'react';
+import styles from './Section.module.scss';
+
+export interface SectionProps extends React.HTMLAttributes<HTMLElement> {
+  type?: 'accent' | 'neutral';
+  bottomSeparator?: boolean;
+  fullHeight?: boolean;
+  children?: React.ReactNode;
+}
+
+export const Section: React.FC<SectionProps> = ({
+  type = 'neutral',
+  bottomSeparator = false,
+  fullHeight = false,
+  children,
+  ...props
+}) => {
+  const classNames = cx(styles.section, {
+    [styles.accent]: type === 'accent',
+    [styles.neutral]: type === 'neutral',
+    [styles.fullHeight]: fullHeight
+  });
+
+  return (
+    <>
+      <section className={classNames} {...props}>
+        {children}
+      </section>
+      {bottomSeparator && <Separator
+          orientation="horizontal"
+          decorative
+          className={styles.separator}
+      />}
+    </>
+  );
+};
