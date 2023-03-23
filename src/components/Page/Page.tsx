527f3536c342bf661413976e5a62af1df22f4217
Andrew Wiggin <andrew@wiggin.dev>
2023-03-23 15:58:10 -0400

    Create Page component

---
+++ src/components/Page/Page.tsx
@@ -0,0 +1,24 @@
+import cx from "classnames";
+import styles from "components/Page/Page.module.scss";
+import React from "react";
+
+export interface PageProps extends React.HTMLAttributes<HTMLElement> {
+  withAffordance?: boolean;
+  children?: React.ReactNode;
+}
+
+export const Page: React.FC<PageProps> = ({
+  withAffordance = true,
+  className,
+  children,
+  ...props
+}) => {
+  const classNames = cx(styles.page, className, {
+    [styles.withAffordance]: withAffordance,
+  });
+  return (
+    <main className={classNames} {...props}>
+      {children}
+    </main>
+  );
+};
--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,26 +1,27 @@
-import cx from 'classnames';
-import {Separator} from "client/radix/Separator";
-import React from 'react';
-import styles from './Section.module.scss';
+import cx from "classnames";
+import { Separator } from "client/radix/Separator";
+import React from "react";
+import styles from "./Section.module.scss";
 
 export interface SectionProps extends React.HTMLAttributes<HTMLElement> {
-  type?: 'accent' | 'neutral';
+  type?: "accent" | "neutral";
   bottomSeparator?: boolean;
   fullHeight?: boolean;
   children?: React.ReactNode;
 }
 
 export const Section: React.FC<SectionProps> = ({
-  type = 'neutral',
+  type = "neutral",
   bottomSeparator = false,
   fullHeight = false,
+  className,
   children,
   ...props
 }) => {
-  const classNames = cx(styles.section, {
-    [styles.accent]: type === 'accent',
-    [styles.neutral]: type === 'neutral',
-    [styles.fullHeight]: fullHeight
+  const classNames = cx(styles.section, className, {
+    [styles.accent]: type === "accent",
+    [styles.neutral]: type === "neutral",
+    [styles.fullHeight]: fullHeight,
   });
 
   return (
@@ -28,11 +29,13 @@ export const Section: React.FC<SectionProps> = ({
       <section className={classNames} {...props}>
         {children}
       </section>
-      {bottomSeparator && <Separator
+      {bottomSeparator && (
+        <Separator
           orientation="horizontal"
           decorative
           className={styles.separator}
-      />}
+        />
+      )}
     </>
   );
 };
