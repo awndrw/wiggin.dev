c7180371e87a8b7e9632381cb1f3fdd10685f31a
Andrew Wiggin <andrew@wiggin.dev>
2023-03-25 12:54:47 -0400

    Enable ESLint during builds

--- next.config.js
+++ next.config.js
@@ -16,10 +16,6 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src")],
   },
-  eslint: {
-    // TODO: Errors in Vercel builds but unable to repro locally
-    ignoreDuringBuilds: true,
-  },
   async headers() {
     const headers = [];
     if (process.env.VERCEL_ENV !== "production") {
