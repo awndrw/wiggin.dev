6670af866157ba55696e596ba59d2043dcf7c78e
Andrew Wiggin <andrew@wiggin.dev>
2023-03-26 03:10:18 -0400

    Revert "Replace NextLink with next-intl Link"
    This reverts commit b43e631ab4aba6f16f3abbb60ca5ba7ed1627422.

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,15 +1,21 @@
+import NextLink from "next/link";
 import React from "react";
 
 import {
   Interaction,
   type InteractionComponentProps,
 } from "components/Interaction";
-import { Link } from "next-intl";
 import { ArrowRight, ArrowUpRight } from "react-feather";
 
-export type InternalLinkProps = InteractionComponentProps<typeof Link>;
+export type InternalLinkProps<T> = InteractionComponentProps<
+  typeof NextLink<T>
+>;
 
-export const InternalLink = ({ children, ...props }: InternalLinkProps) => {
+export const InternalLink = <T extends string>({
+  children,
+  ...props
+}: InternalLinkProps<T>) => {
+  const Link = NextLink<T>;
   return (
     <Interaction component={Link} icon={ArrowRight} {...props}>
       {children}
