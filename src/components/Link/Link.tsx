03424024a812cb43d9332a413c2dba50048434ec
Andrew Wiggin <andrew@wiggin.dev>
2023-03-26 03:13:31 -0400

    Improve Link typing

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -11,31 +11,27 @@ export type InternalLinkProps<T> = InteractionComponentProps<
   typeof NextLink<T>
 >;
 
-export const InternalLink = <T extends string>({
-  children,
-  ...props
-}: InternalLinkProps<T>) => {
+export function InternalLink<T>({ children, ...props }: InternalLinkProps<T>) {
   const Link = NextLink<T>;
   return (
     <Interaction component={Link} icon={ArrowRight} {...props}>
       {children}
     </Interaction>
   );
-};
+}
 
 export type ExternalLinkProps = InteractionComponentProps<"a">;
 
-export const ExternalLink: React.FC<ExternalLinkProps> = ({
-  children,
-  ...props
-}) => (
-  <Interaction
-    component="a"
-    icon={ArrowUpRight}
-    target="_blank"
-    rel="noreferrer"
-    {...props}
-  >
-    {children}
-  </Interaction>
-);
+export function ExternalLink({ children, ...props }: ExternalLinkProps) {
+  return (
+    <Interaction
+      component="a"
+      icon={ArrowUpRight}
+      target="_blank"
+      rel="noreferrer"
+      {...props}
+    >
+      {children}
+    </Interaction>
+  );
+}
