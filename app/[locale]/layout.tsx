f26ddf9860cf65bfc8b3d70c6c18ccf77caa9444
Andrew Wiggin <andrew@wiggin.dev>
2023-03-27 20:00:12 -0400

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -71,7 +71,7 @@ export default async function RootLayout({
   return (
     <html lang={locale}>
       <head>
-        <Partytown debug forward={["dataLayer.push"]} />
+        <Partytown debug={env !== "production"} forward={["dataLayer.push"]} />
         <Script id={getId()} type="text/partytown">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NM2JLN5');`}
         </Script>
