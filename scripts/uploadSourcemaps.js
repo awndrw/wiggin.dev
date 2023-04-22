00108925807f78078fbcd5802da4f5f8256a4650
Andrew Wiggin <andrew@wiggin.dev>
2023-04-22 10:28:22 -0400

    Add repository url to datadog sourcemap upload

--- scripts/uploadSourcemaps.js
+++ scripts/uploadSourcemaps.js
@@ -8,7 +8,8 @@ exec(
   `yarn dlx @datadog/datadog-ci sourcemaps upload .next/static/chunks \
 --service=wiggin.dev \
 --release-version=${version} \
---minified-path-prefix=/_next/static/chunks`,
+--minified-path-prefix=/_next/static/chunks \
+--repository-url=https://github.com/wiggindev/wiggin.dev`,
   process.env,
   (error, stdout, stderr) => {
     if (error) {
