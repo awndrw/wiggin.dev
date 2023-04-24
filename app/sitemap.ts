0217625742ce733ac3af182adb09f12adb802da9
Andrew Wiggin <andrew@wiggin.dev>
2023-04-23 22:16:39 -0400

    Add hue dynamic path to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ import { url } from "constants/url";
 export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
   const filePaths = await globby(["app/**/page.tsx"]);
   return filePaths.map((filePath) => {
-    const path = filePath.match(/^app\/(.+?)\/page\.tsx$/)?.[1] ?? "";
+    const path = filePath.match(/^app\/\[hue]\/(.+?)\/page\.tsx$/)?.[1] ?? "";
     return { url: new URL(path, url).href };
   });
 }
