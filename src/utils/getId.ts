e2395a94514f99575aabe466110d6a7f00bbd00e
Andrew Wiggin <andrew@wiggin.dev>
2023-05-20 12:36:24 -0400

    Update getId.ts

--- src/utils/getId.ts
+++ src/utils/getId.ts
@@ -1,13 +1,8 @@
 import "server-only";
 
-const ids = new Set();
-
 const prefix = String.fromCharCode(97 + Math.floor(Math.random() * 26));
+let index = 0;
 
 export function getId(): string {
-  const id = Math.random().toString(36).substring(2, 7);
-  if (ids.has(id)) {
-    return getId();
-  }
-  return prefix + id;
+  return `${prefix}${index++}`;
 }
