c08f9bfac7ebe8fd6cf1f66c4ef47044dad7c576
Andrew Wiggin <andrew@wiggin.dev>
2023-05-20 01:09:16 -0400

    Update Text.tsx

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,14 +1,22 @@
 import cx from "classnames";
-import { type ComponentProps, type FC } from "react";
+import {
+  type ComponentPropsWithoutRef,
+  type ForwardedRef,
+  forwardRef,
+} from "react";
 
 import styles from "./Text.module.scss";
 
-export type TextProps = ComponentProps<"p">;
+export type TextProps = ComponentPropsWithoutRef<"p">;
 
-export const Text: FC<TextProps> = ({ children, className, ...props }) => {
+export const Text = forwardRef(TextImpl);
+function TextImpl(
+  { children, className, ...props }: TextProps,
+  ref: ForwardedRef<HTMLParagraphElement>
+) {
   return (
-    <p className={cx(styles.text, className)} {...props}>
+    <p className={cx(styles.text, className)} ref={ref} {...props}>
       {children}
     </p>
   );
-};
+}
