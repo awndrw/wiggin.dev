0a24ddb889b0d4a3f22b511002d377181d0c8037
Andrew Wiggin <andrew@wiggin.dev>
2023-05-20 01:00:37 -0400

    Remove playground

--- app/[hue]/playground/10-things/config.ts
+++
@@ -1,9 +0,0 @@
-import previewPng from "../10-things/preview.png";
-import { type PlaygroundConfig } from "../utils";
-
-export default {
-  name: 'Google\'s "Ten things"',
-  preview: previewPng.src,
-  tags: ["desktop"],
-  warning: "This playground is not accessible or mobile-friendly.",
-} satisfies PlaygroundConfig;
