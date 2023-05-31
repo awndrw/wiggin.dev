8bfea2a0d51ad7c28e13e679e6de9c99120b26cd
Andrew Wiggin <andrew@wiggin.dev>
2023-05-31 19:54:58 -0400

    Improve contrast

--- src/theme/oklch.ts
+++ src/theme/oklch.ts
@@ -5,7 +5,7 @@ export const lightnessAndChromaValues: Record<
   Record<Color, readonly [number, number]>
 > = {
   light: {
-    primary: [0.56, 0.15],
+    primary: [0.4, 0.15],
     "primary-contrast": [0.99, 0.005],
     container: [0.27, 0.06],
     "container-contrast": [0.93, 0.03],
@@ -14,7 +14,7 @@ export const lightnessAndChromaValues: Record<
   },
   dark: {
     primary: [0.88, 0.064],
-    "primary-contrast": [0.35, 0.1],
+    "primary-contrast": [0.3, 0.1],
     container: [0.93, 0.03],
     "container-contrast": [0.45, 0.1],
     tint: [0.28, 0.02],
