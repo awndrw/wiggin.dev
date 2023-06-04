af388db7f0e31ca206716a45f3149c65362feeba
Andrew Wiggin <andrew@wiggin.dev>
2023-06-04 13:12:17 -0400

    Remove mutate util

--- src/theme/utils/createCSSVars.ts
+++ src/theme/utils/createCSSVars.ts
@@ -1,11 +1,14 @@
-import { type Hue, type Mode } from "theme/constants";
+import { type Color, type Hue, type Mode } from "theme/constants";
 import { lightnessAndChromaValues } from "theme/oklch";
 import { oklch } from "theme/utils";
-import { mutate } from "utils/mutate";
 
 export function createCSSVars(hue: Hue, mode: Mode) {
-  const map = mutate(lightnessAndChromaValues[mode], ([lightness, chroma]) =>
-    oklch(lightness, chroma, hue)
+  const map = Object.entries(lightnessAndChromaValues[mode]).reduce(
+    (entries, [name, [lightness, chroma]]) => {
+      entries[name as Color] = oklch(lightness, chroma, hue);
+      return entries;
+    },
+    {} as Record<Color, string>
   );
   return Object.entries(map)
     .map(([name, color]) => `--color-accent-${name}: ${color};`)
