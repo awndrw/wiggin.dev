d2d3a5cfee96b1ef8d9304ebe4b9ff12ed92762a
Andrew Wiggin <andrew@wiggin.dev>
2024-01-14 21:24:47 -0500

    x

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,28 @@
 import { type Metadata } from "next";
 import { type ReactNode } from "react";
-import { z } from "zod";
 
 import { host, url } from "constants/url";
-import { getEdgeConfig } from "utils/getEdgeConfig";
 
 import "./globals.scss";
 
-export async function generateMetadata(): Promise<Metadata> {
-  const tagline = await getEdgeConfig("tagline", z.string());
-  return {
-    title: {
-      default: host,
-      template: `${host}/%s`,
-    },
-    description: `Andrew Wiggin is a ${tagline}.`,
-    metadataBase: url,
-    openGraph: {
-      type: "website",
-      title: "Andrew Wiggin",
-      description: "",
-    },
-    twitter: {
-      card: "summary",
-      site: "@wiggindev",
-    },
-  };
-}
+export const metadata: Metadata = {
+  title: {
+    default: host,
+    template: `${host}/%s`,
+  },
+  description:
+    "Andrew Wiggin is a Brooklyn based design engineer passionate about interaction, accessibility, and design systems.",
+  metadataBase: url,
+  openGraph: {
+    type: "website",
+    title: "Andrew Wiggin",
+    description: "",
+  },
+  twitter: {
+    card: "summary",
+    site: "@wiggindev",
+  },
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return children;
--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,18 +1,6 @@
 import { env } from "constants/env";
 
-const PRODUCTION_HOST = "wiggin.dev";
-const PREPROD_HOST = "dev.wiggin.dev";
-const PREVIEW_HOST =
-  process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL || PREPROD_HOST;
-const DEVELOPMENT_HOST = "localhost:3000";
-
-export const host = env.isProduction
-  ? PRODUCTION_HOST
-  : process.env.IS_PREPROD
-  ? PREPROD_HOST
-  : env.isPreview
-  ? PREVIEW_HOST
-  : DEVELOPMENT_HOST;
+export const host = "legacy.awndrw.com";
 
 const protocol = env.isDevelopment ? "http://" : "https://";
 export const url = new URL(protocol + host);
